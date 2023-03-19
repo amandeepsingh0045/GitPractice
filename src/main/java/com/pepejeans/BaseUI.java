2938c0375f67335af742b6ff3f905a9ac3121f70
Amandeep Singh <amandeepsingh0045@gmail.com>
2023-03-19 22:44:38 +0530

    first commit

---
+++ src/main/java/com/pepejeans/BaseUI.java
@@ -0,0 +1,40 @@
+package com.pepejeans;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class BaseUI {
+
+    public WebDriver driver = null;
+
+    @Test
+    public void invokeBrowser() {
+
+            /*    System.setProperty("webdriver.chrome.driver",
+                        System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
+                driver = new ChromeDriver();
+
+             */
+
+                System.setProperty("webdriver.gecko.driver",
+                        System.getProperty("user.dir") + "\\src\\main\\resources\\geckodriver.exe");
+                driver = new FirefoxDriver();
+
+            driver.get("https://www.pepejeans.in");
+            driver.manage().window().maximize();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+            String title =driver.getTitle();
+            System.out.println(title);
+            driver.quit();
+
+    }
+    }
+
+
